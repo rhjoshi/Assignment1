3e5745c2d6df1a3ac9a6d74664091fe3daff7286
rhjoshi <rajanjoshi60@yahoo.com>
2014-08-29 10:35:18 -0700

    Assignment
    assignment

---
+++ Assignment1.cpp
@@ -0,0 +1,50 @@
+﻿/* This C program demonstrates the switch statement without using breaks. */
+/* The program is tested on MS Visual C++ platform */
+#include <stdio.h>
+#include<stdlib.h>
+using namespace std;
+#pragma warning(disable : 4996) 
+void main() {
+	char ch = '+';
+	int f, a = 10, b = 20;
+	printf("ch = %c\n", ch);
+	switch (ch) {
+	case '+': f = a + b; printf("f = %d\n", f);
+	case '−': f = a - b; printf("f = %d\n", f);
+	case '*': f = a * b; printf("f = %d\n", f);
+	case '/': f = a / b; printf("f = %d\n", f);
+	default: printf("invalid operator\n");
+		break;
+	}
+	ch = '−';
+	printf("ch = %c\n", ch);
+	switch (ch) {
+	case '+': f = a + b; printf("f = %d\n", f);
+	case '−': f = a - b; printf("f = %d\n", f);
+	case '*': f = a * b; printf("f = %d\n", f);
+	case '/': f = a / b; printf("f = %d\n", f);
+	default: printf("invalid operator\n");
+		break;
+	}
+	ch = '*';
+	printf("ch = %c\n", ch);
+	switch (ch) {
+	case '+': f = a + b; printf("f = %d\n", f);
+	case '−': f = a - b; printf("f = %d\n", f);
+	case '*': f = a * b; printf("f = %d\n", f);
+	case '/': f = a / b; printf("f = %d\n", f);
+	default: printf("invalid operator\n");
+		break;
+	}
+	ch = '/';
+	printf("ch = %c\n", ch); 
+	switch (ch) {
+		case '+': f = a + b; printf("f = %d\n", f);
+		case '−': f = a - b; printf("f = %d\n", f);
+		case '*': f = a * b; printf("f = %d\n", f);
+		case '/': f = a / b; printf("f = %d\n", f);
+		default: printf("invalid operator\n");
+			break;
+	}
+	system("pause");
+}
